2c5255002f1ef3e64600aeea9abea78c4e0ef8be
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-09-11 12:19:01 +0900

    주석 제거

--- Uni_Name/index.js
+++ Uni_Name/index.js
@@ -44,30 +44,20 @@ axios.get(url)
             const html = response.data;
             const $ = cheerio.load(html);
 
-            // 대학 이름을 저장할 배열 생성
             const universityNames = [];
 
-            // 웹 페이지에서 대학 이름을 추출하여 배열에 추가
             $('').each((index, element) => {
                 const universityName = $(element).text();
                 universityNames.push(universityName);
-                // #nowResults > li: nth - child(1) > span: nth - child(1) > a: nth - child(2)
-                // 여기에 적절한 CSS 선택자를 사용하여 대학 이름을 선택하십시오.
-                // 예를 들어, 웹 페이지의 대학 이름이 <h2> 태그 안에 있다면 다음과 같이 선택할 수 있습니다:
-                // const universityName = $(element).text();
-                // universityNames.push(universityNames);
             });
 
-            // 엑셀 파일 생성 및 데이터 추가
             const workbook = new ExcelJS.Workbook();
-            // const workbook = new ExcelJS.Workbook();
             const worksheet = workbook.addWorksheet('University Names');
 
             universityNames.forEach((name) => {
                 worksheet.addRow([name]);
             });
 
-            // 엑셀 파일 저장
             const excelFilePath = 'university_names.xlsx';
             workbook.xlsx.writeFile(excelFilePath)
                 .then(() => {
